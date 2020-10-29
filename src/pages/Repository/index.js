c001d2c40f7b3cbf3220378853c004bd2080eed6
Lucas Boullosa <lucaskmboulhosa@gmail.com>
2020-10-29 01:54:59 +0000

    finished style and navigation

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,7 +1,22 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
+
+import Container from '../../components/Container'
+import { Loading, Owner, IssuesList } from './styles';
+
+import { FaSpinner } from 'react-icons/fa';
+import PropTypes from 'prop-types';
 import api from '../../services/api';
 
 class Repository extends Component {
+	static propTypes = {
+		match: PropTypes.shape({
+			params: PropTypes.shape({
+				repository: PropTypes.string,
+			}),
+		}).isRequired,
+	};
+
 	state = {
 		loading: true,
 		repository: {},
@@ -20,8 +35,8 @@ class Repository extends Component {
 					per_page: 5,
 				},
 			}),
-    ]);
-    
+		]);
+
 		this.setState({
 			loading: false,
 			repository: repository.data,
@@ -31,7 +46,44 @@ class Repository extends Component {
 	}
 
 	render() {
-		return <div>Repository:</div>;
+		const { loading, repository, issues } = this.state;
+
+		if (loading) {
+			return (
+				<Loading loading={loading ? 1 : 0}>
+					<span>Loading</span>
+					<FaSpinner />
+				</Loading>
+			);
+		}
+
+		return (
+		<Container>
+			<Owner>
+				{console.log(repository)}
+				<Link to='/'>Back to main page</Link>			
+				<img src ={repository.owner.avatar_url} alt='Repository owner avatar' />
+				<h1>{repository.name}</h1>
+				<p>{repository.description}</p>
+			</Owner>			
+
+			<IssuesList>
+				{issues.map(issue => (
+					<li key={String(issue.id)}>
+						<img src={issue.user.avatar_url} alt={issue.user.login} />
+						<div>
+							<strong>
+								<a href={issue.html_url}>{issue.title}</a>
+								{issue.labels.map(label => (
+									<span key={String(label.id)}>{label.name}</span>
+								))}
+							</strong>
+							<p>{issue.user.login}</p>
+						</div>
+					</li>
+				))}
+			</IssuesList>
+		</Container>);
 	}
 }
 
